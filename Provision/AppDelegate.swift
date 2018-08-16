51aca70388d7cd4cbf0ede55d2c661f89eb5f96b
Dalton Claybrook <daltonclaybrook@gmail.com>
2018-08-15 23:23:41 -0400

    Initial commit

---
+++ Provision/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Provision
+//
+//  Created by Dalton Claybrook on 8/15/18.
+//  Copyright © 2018 Claybrook Software, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Provision/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Provision
+//
+//  Created by Dalton Claybrook on 8/15/18.
+//  Copyright © 2018 Claybrook Software, LLC. All rights reserved.
+//
+
+import Cocoa
+import Security
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let profileURL = Bundle.main.url(forResource: "profile", withExtension: "mobileprovision")!
+    let data = try! Data(contentsOf: profileURL)
+    let profile = try! Profile(signedData: data)
+    print("profile: \(profile)")
+  }
+}
+
